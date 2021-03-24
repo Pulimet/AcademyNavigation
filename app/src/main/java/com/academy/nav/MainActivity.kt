260298070022c7b16785b73990dd2a66e6da5f12
Pulimet <korolev2009@gmail.com>
2021-03-24 18:24:56 +0200

    Added solution for workshop 1

--- app/src/main/java/com/academy/nav/MainActivity.kt
+++ app/src/main/java/com/academy/nav/MainActivity.kt
@@ -1,49 +1,28 @@
 package com.academy.nav
 
-import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.os.bundleOf
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.add
-import androidx.fragment.app.commit
-import androidx.fragment.app.replace
-import com.academy.nav.ui.details.DetailsFragment
-import com.academy.nav.ui.home.HomeFragment
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.academy.nav.ui.navigation.Destination
 import com.academy.nav.ui.navigation.NavigationViewModel
-import com.academy.nav.ui.settings.SettingsFragment
-import com.academy.ui_favorites.FavoritesFragment
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
     private val navViewModel: NavigationViewModel by viewModels()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        if (savedInstanceState == null) {
-            supportFragmentManager.commit {
-                setReorderingAllowed(true)
-                add<HomeFragment>(R.id.fragment_container_view)
-            }
-        }
-    }
+    private lateinit var navController: NavController
 
     override fun onStart() {
         super.onStart()
+        navController = findNavController(R.id.fragment_container_view)
+
         navViewModel.getNavEvent().observe(this) {
             when (it.destination) {
-                Destination.DETAILS -> replace<DetailsFragment>(bundleOf("movieId" to it.movie?.id))
-                Destination.SETTINGS -> replace<SettingsFragment>()
-                Destination.FAVORITES -> replace<FavoritesFragment>()
+                Destination.DETAILS -> navController.navigate(R.id.detailsFragment, bundleOf("movieId" to it.movie?.id))
+                Destination.SETTINGS -> navController.navigate(R.id.settingsFragment)
+                Destination.FAVORITES -> navController.navigate(R.id.favoritesFragment)
             }
-        }
-    }
-
-    private inline fun <reified T : Fragment> replace(bundle: Bundle? = null) {
-        supportFragmentManager.commit {
-            setReorderingAllowed(true)
-            addToBackStack(T::class.java.simpleName)
-            replace<T>(R.id.fragment_container_view, args = bundle)
         }
     }
 }
