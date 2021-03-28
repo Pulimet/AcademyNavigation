2a546f4ce2d783eb7647547fe31b24bdfc72abcd
Pulimet <korolev2009@gmail.com>
2021-03-28 19:12:37 +0300

    Added solution for workshop 3 - Removed unused code

--- app/src/main/java/com/academy/nav/ui/login/LoginFragment.kt
+++ app/src/main/java/com/academy/nav/ui/login/LoginFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.SavedStateHandle
 import androidx.navigation.fragment.findNavController
 import com.academy.nav.R
 import com.academy.nav.databinding.FragmentLoginBinding
@@ -12,19 +11,11 @@ import com.academy.nav.ui.binding.FragmentBinding
 import com.google.android.material.snackbar.Snackbar
 
 class LoginFragment : Fragment(R.layout.fragment_login), View.OnClickListener {
-    companion object {
-        const val LOGIN_SUCCESSFUL: String = "LOGIN_SUCCESSFUL"
-    }
 
     private val binding by FragmentBinding(FragmentLoginBinding::bind)
-
     private val loginViewModel: LoginViewModel by activityViewModels()
-    private lateinit var savedStateHandle: SavedStateHandle
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        savedStateHandle = findNavController().previousBackStackEntry!!.savedStateHandle
-        savedStateHandle.set(LOGIN_SUCCESSFUL, false)
-
         binding.btnLogin.setOnClickListener(this)
     }
 
@@ -32,7 +23,6 @@ class LoginFragment : Fragment(R.layout.fragment_login), View.OnClickListener {
     private fun login(username: String, password: String) {
         loginViewModel.login(username, password).observe(viewLifecycleOwner, { success ->
             if (success) {
-                savedStateHandle.set(LOGIN_SUCCESSFUL, true)
                 findNavController().popBackStack()
             } else {
                 showErrorMessage()
